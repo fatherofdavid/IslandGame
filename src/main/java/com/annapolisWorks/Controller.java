904d29c498a8ef998f972da2737aff889769e780
Jesse Taylor <fatherofdavid@msn.com>
2016-11-21 14:50:49 -0500

    adding basic GUI-model interactions before splitting tracks between dev teams

--- src/main/java/com/annapolisWorks/Controller.java
+++ src/main/java/com/annapolisWorks/Controller.java
@@ -1,4 +1,15 @@
 package com.annapolisWorks;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    Button button1 = new Button();
+
+    public void doMyAction(ActionEvent event){
+        System.out.println("I've been clicked!");
+    }
 }
