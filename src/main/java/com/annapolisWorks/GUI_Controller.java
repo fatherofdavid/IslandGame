7821ca3baa21b376bd5d8c6db814328df7df5a38
Iessey <jss>
2016-11-22 12:04:57 -0500

    committing new changes to pull in from model_development

--- src/main/java/com/annapolisWorks/GUI_Controller.java
+++ src/main/java/com/annapolisWorks/GUI_Controller.java
@@ -6,6 +6,20 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class GUI_Controller {
+    Label currentPlayerLabel;
+
+    @FXML
+    private Label player1Label;
+
+    @FXML
+    private Label player2Label;
+
+    @FXML
+    private Label player3Label;
+
+    @FXML
+    private Label player4Label;
+
 
     @FXML
     Label remainingActionsLabel;
@@ -34,6 +48,10 @@ public class GUI_Controller {
     @FXML
     private Button useCardButton;
 
+    public void startUp() {
+        currentPlayerLabel = player1Label;
+    }
+
     public void actionUsed(float used) {
         float remainingActions = Float.parseFloat(remainingActionsLabel.getText());
         remainingActions = remainingActions - used;
@@ -44,9 +62,20 @@ public class GUI_Controller {
         flyButton.setDisable(true);
     }
 
+    public void nextTurn(Adventurer activePlayer) {
+        String id = activePlayer.getName();
+        currentPlayerLabel.setText(currentPlayerLabel.getText().substring(1));
+        if(id == player1Label.getText()) player1Label.setText(">" + player1Label.getText());
+        else if(id == player2Label.getText()) player1Label.setText(">" + player1Label.getText());
+        else if(id == player3Label.getText()) player1Label.setText(">" + player1Label.getText());
+        else if(id == player4Label.getText()) player1Label.setText(">" + player1Label.getText());
+        else throw new RuntimeException("attempting to switch turn to non-existent player");
+
+        //draw right-panel buttons  and windows correctly for new player
+    }
+
     //user button commands
     public void move(ActionEvent event){
-        Main.test(this);
     }
     public void shoreUp(ActionEvent event){
     }
